0ade086f8912b23a04332b9c2f40c52f2b8e6ddd
louwenus <louwen.fricout@gmail.com>
2025-02-25 17:14:28 +0100

    il est méchant

--- impl_r/tomasulo_parrallel/tests/queue.rs
+++ impl_r/tomasulo_parrallel/tests/queue.rs
@@ -1,8 +1,7 @@
 use std::{
     ptr::null_mut,
     sync::atomic::{
-        AtomicPtr, AtomicU64,
-        Ordering::{Acquire, Relaxed, Release},
+        AtomicPtr, AtomicU64, AtomicUsize, Ordering::{Acquire, Relaxed, Release}
     },
 };
 use tomasulo_parrallel::atomic_queue::{AtomicQueue, QueueElem};
@@ -11,16 +10,51 @@ use std::thread;
 const NUM_THREAD: i8 = 5;
 const NUM_ITERATION: i64 = 1000000;
 
-static cnt : AtomicU64 = AtomicU64::new(0);
+static CNT : AtomicUsize = AtomicUsize::new(0);
 
 
 
-fn pusher(queue : &AtomicQueue<u64>) {
-    let cur = 
+fn pusher(queue : &AtomicQueue<i64>, & elems:&Vec<QueueElem<i64>>) {
+    let mut i = 0;
+    while i<NUM_ITERATION {
+        let cur = CNT.fetch_add(1, Relaxed);
+        queue.push(&mut (elems[cur]));
+        i+=1;
+    }
+}
+
+fn collect_q(queue : &AtomicQueue<i64>){
+    let mut i = 0_i64;
+    while i < NUM_ITERATION*NUM_THREAD as i64 {
+        let mut ret = None;
+        while ret.is_none() {
+            ret = queue.pop();
+        }
+        unsafe {
+            let elem = ret.unwrap_unchecked();
+            assert_ne!(elem.data,-1);
+            elem.data = -1;
+        }
+        i+=1;
+    }
 }
 
 #[test]
 fn concurent_usage() -> () {
-    let queue = AtomicQueue::<u64>::new();
-    pusher(queue);
+    let queue = AtomicQueue::<i64>::new();
+    let mut elems :Vec<_>= (0..NUM_ITERATION*NUM_THREAD as i64).map(
+        |i| {
+            QueueElem::new(i)
+        }
+    ).collect();
+    let collector = thread::spawn(move || {
+        collect_q(queue.as_ref());
+    });
+    let pushers :Vec<_> = (0..NUM_THREAD).map(
+        |_| {
+            thread::spawn(move || {
+                pusher(queue.as_ref(), & elems);
+            })
+        }
+    ).collect();
 }
